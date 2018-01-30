e96b85269bb0eb3bfcea4366a3b78bbe81dd14d8
viking den <dengwj@gionee.com>
2018-01-30 13:53:31 +0800

    add Longest_Substring_Without_Repeating_Characters's python implementation

--- Python/S003_Longest_Substring_Without_Repeating_Characters.py
+++ Python/S003_Longest_Substring_Without_Repeating_Characters.py
@@ -3,9 +3,22 @@
 
 
 def solution():
-    content = 'abcefggesf'
+    content = 'abcefg'
     solutionBasic(content)
-    #solutionByMap(content)
+    solutionByMap(content)
+
+
+def solutionByMap(content):
+    print('------------------solution by Map------------------------')
+    answer = 0
+    container = {}
+    j = 0 ;
+    for i in range(len(content)):
+        if content[i] in container:
+            j = j if j > container[content[i]] else container[content[i]]
+        answer = answer if answer > i - j + 1 else i - j + 1
+        container[content[i]] = i + 1
+    print('answer : {}'.format(answer))
 
 
 def solutionBasic(content):
