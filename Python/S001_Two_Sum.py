6a97ed811d8caddea32888ae09a29b54b2faf9ca
Viking Den <vikingden7@gmail.com>
2019-07-28 09:06:16 +0800

    update project

--- Python/S001_Two_Sum.py
+++ Python/S001_Two_Sum.py
@@ -1,26 +1,55 @@
 #!/usr/bin/python3
 #coding=utf-8
 
-def solution():
-    array = [2, 7, 11, 15, 34, 5, 61, 9]
+def solution_force(arr, target):
+    """
+    暴力破解法
+
+    对于给定的target，通过两个循环来遍历数组，故时间复杂度为O（n^2）。
+
+    """
     result = [-1, -1]
-    target = 26
+    size = len(arr)
+    hasSolution = False
+    for i, item in enumerate(arr):
+        j = i + 1
+        while j < size:
+            if target == item + arr[j]:
+                result = [i, j]
+                hasSolution = True
+                break
+            j += 1
+        if hasSolution:
+            break
+    if hasSolution:
+        print("Soultion:{} {}".format(result[0], result[1]))
+    else:
+        print('No Two Sum Solution.')
+
+def solution(arr, target):
+    """
+    使用字典的方式，时间复杂度O(n)
+    """
+    result = [-1, -1]
     container = {}
     hasSolution = False
-    for index in range(len(array)):
-        if target - array[index] in container:
+    for index in range(len(arr)):
+        if target - arr[index] in container:
             # bingo , we got it
             hasSolution = True
-            result[0] = container[target- array[index]]
+            result[0] = container[target- arr[index]]
             result[1] = index
             break
         else:
             # no match, put it to container
-            container[array[index]] = index
+            container[arr[index]] = index
     if hasSolution:
         print("Soultion:{} {}".format(result[0], result[1]))
     else:
         print('No Two Sum Solution.')
 
 if __name__ == '__main__':
-    solution()
+    array = [2, 7, 11, 15, 34, 5, 61, 9]
+    target = 26
+    solution_force(array, target)
+    # solution(array, target)
--- Solutions/S001_Two_Sum.md
+++ Solutions/S001_Two_Sum.md
@@ -1,25 +1,22 @@
-### 01 Two Sum
+### 001 两数之和（Two Sum）
 
 #### Description
 
-Given an array of integers, return **indices** of the two numbers such that they add up to a specific target.
+给定一个整数数组 **nums** 和一个目标值 **target**，请你在该数组中找出和为目标值的那 **两个** 整数，并返回他们的数组下标。
 
-You may assume that each input would have **exactly** one solution, and you may not use the same element twice.
+你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
 
-Example:
+示例:
 
 ```
-Given nums = [2, 7, 11, 15], target = 9,
 
-Because nums[0] + nums[1] = 2 + 7 = 9,
+给定 nums = [2, 7, 11, 15], target = 9
 
-return [0, 1].
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]
+
 ```
 
 #### Implementations
 
-[Java](../Java/src/main/java/com/viking/leetcode/S001_Two_Sum.java)
-
 [Python](../Python/S001_Two_Sum.py)
-
-[C](S000_Not_Implemented_Yet.md)
